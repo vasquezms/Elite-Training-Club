a97df12dd9bc6aa55a7d8ff63ecffe1b5b76fba1
SANTIAGO MU�OZ VASQUEZ <santiagomunoz298536@correo.itm.edu.co>
2022-04-07 22:57:55 -0500

    DetailsCountry

--- Elite_Training_Club/Elite_Training_Club/Controllers/CountriesController.cs
+++ Elite_Training_Club/Elite_Training_Club/Controllers/CountriesController.cs
@@ -20,13 +20,13 @@ namespace Elite_Training_Club.Controllers
             _context = context;
         }
 
-        // GET: Countries
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             return View(await _context.Countries.ToListAsync());
         }
 
-        // GET: Countries/Details/5
+        [HttpGet]
         public async Task<IActionResult> Details(int? id)
         {
             if (id == null)
@@ -34,7 +34,7 @@ namespace Elite_Training_Club.Controllers
                 return NotFound();
             }
 
-            var country = await _context.Countries
+            Country country = await _context.Countries
                 .FirstOrDefaultAsync(m => m.Id == id);
             if (country == null)
             {
@@ -44,18 +44,16 @@ namespace Elite_Training_Club.Controllers
             return View(country);
         }
 
-        // GET: Countries/Create
+        [HttpGet]
         public IActionResult Create()
         {
             return View();
         }
 
-        // POST: Countries/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+      
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name")] Country country)
+        public async Task<IActionResult> Create( Country country)
         {
             if (ModelState.IsValid)
             {
