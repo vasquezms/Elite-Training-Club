00669a9a948b15b7ed1a9c77b63073e58d6f3efd
SANTIAGO MU�OZ VASQUEZ <santiagomunoz298536@correo.itm.edu.co>
2022-04-09 01:01:50 -0500

    plans Crud finish

---
+++ Elite_Training_Club/Elite_Training_Club/Migrations/20220409045637_AddPlanEntity.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Elite_Training_Club.Migrations
+{
+    public partial class AddPlanEntity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Plans",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Plans", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Plans_Name",
+                table: "Plans",
+                column: "Name",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Plans");
+        }
+    }
+}
