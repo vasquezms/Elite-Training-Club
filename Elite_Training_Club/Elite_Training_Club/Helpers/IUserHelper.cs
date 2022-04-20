33c3ee0a7e8d8bd9c075e0d5baa0929af1d93226
HERNAN ALONSO BERRIO BETANCUR <hernanberrio274803@correo.itm.edu.co>
2022-04-20 01:32:50 -0500

    Funtional

---
+++ Elite_Training_Club/Elite_Training_Club/Helpers/IUserHelper.cs
@@ -0,0 +1,19 @@
+﻿using Elite_Training_Club.Data.Entities;
+using Microsoft.AspNetCore.Identity;
+
+namespace Elite_Training_Club.Helpers
+{
+    public interface IUserHelper
+    {
+        Task<User> GetUserAsync(string email);
+
+        Task<IdentityResult> AddUserAsync(User user, string password);
+
+        Task CheckRoleAsync(string roleName);
+
+        Task AddUserToRoleAsync(User user, string roleName);
+
+        Task<bool> IsUserInRoleAsync(User user, string roleName);
+
+    }
+}
