66936cf9b5ca98a989578b3b6e03f10c1c1ebb2a
HERNAN ALONSO BERRIO BETANCUR <hernanberrio274803@correo.itm.edu.co>
2022-05-17 21:29:39 -0500

    imges zoom & Improved Home

--- Elite_Training_Club/Elite_Training_Club/Controllers/HomeController.cs
+++ Elite_Training_Club/Elite_Training_Club/Controllers/HomeController.cs
@@ -23,37 +23,13 @@ namespace Elite_Training_Club.Controllers
 
         public async Task<IActionResult> Index()
         {
-            List<Product>? products = await _context.Products
+            List<Product> products = await _context.Products
                 .Include(p => p.ProductImages)
                 .Include(p => p.ProductCategories)
                 .OrderBy(p => p.Description)
                 .ToListAsync();
-            List<ProductsHomeViewModel> productsHome = new() { new ProductsHomeViewModel() };
-            int i = 1;
-            foreach (Product? product in products)
-            {
-                if (i == 1)
-                {
-                    productsHome.LastOrDefault().Product1 = product;
-                }
-                if (i == 2)
-                {
-                    productsHome.LastOrDefault().Product2 = product;
-                }
-                if (i == 3)
-                {
-                    productsHome.LastOrDefault().Product3 = product;
-                }
-                if (i == 4)
-                {
-                    productsHome.LastOrDefault().Product4 = product;
-                    productsHome.Add(new ProductsHomeViewModel());
-                    i = 0;
-                }
-                i++;
-            }
 
-            HomeViewModel model = new() { Products = productsHome };
+            HomeViewModel model = new() { Products = products };
             User user = await _userHelper.GetUserAsync(User.Identity.Name);
             if (user != null)
             {
@@ -63,11 +39,14 @@ namespace Elite_Training_Club.Controllers
             }
 
             return View(model);
-
         }
 
 
-        public async Task<IActionResult> Add(int? id)
+
+    
+
+
+    public async Task<IActionResult> Add(int? id)
         {
             if (id == null)
             {
--- Elite_Training_Club/Elite_Training_Club/Models/HomeViewModel.cs
+++ Elite_Training_Club/Elite_Training_Club/Models/HomeViewModel.cs
@@ -1,9 +1,11 @@
-﻿namespace Elite_Training_Club.Models
+﻿using Elite_Training_Club.Data.Entities;
+
+namespace Elite_Training_Club.Models
 {
 
     public class HomeViewModel
     {
-        public ICollection<ProductsHomeViewModel> Products { get; set; }
+        public ICollection<Product> Products { get; set; }
 
         public float Quantity { get; set; }
     }
